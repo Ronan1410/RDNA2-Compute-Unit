49ce93a2a5318dd753ebfc7c479b1a6a30ea9a3f
Ronan1410 <1410ronan@gmail.com>
2026-09-14 10:36:29 +0530

    made a simple 2 bit CSA

--- src/Sample files/new/CSA/CSA_2bit.v
+++ src/Sample files/new/CSA/CSA_2bit.v
@@ -1,11 +1,11 @@
 //Carry Save Adder
 
-`timescale 1ns/ 1ps
+`timescale 1ns / 1ps
 
 module CSA(
-    input wire [1:0] A,
-    input wire [1:0] B,
-    output wire [1:0] Sum,
+    input wire[1:0] A,
+    input wire[1:0] B,
+    output wire[1:0] Sum,
     output wire Cout
     );
 
@@ -17,7 +17,12 @@ assign temp_c1[0] = 1'b0;
 fulladder temp_bit0(.A(A[0]), .B(B[0]), .Cin(1'b0), .Sum(temp_sum1[0]), .Cout(temp_c1[1]));
 fulladder temp_bit1(.A(A[1]), .B(B[1]), .Cin(1'b0), .Sum(temp_sum1[1]), .Cout(temp_c1[2]));
 
+//fulladder bit0(.A(temp_sum1[0]), .B(1'b0), .Cin(1'b0), .Sum(temp_sum2[0]), .Cout(temp_c2[0]));
+//fulladder bit1(.A(temp_sum1[1]), .B(temp_c1[0]), .Cin(temp_c2[0]), .Sum(temp_sum2[1]), .Cout(temp_c2[1]));
+//fulladder bit2(.A(1'b0), .B(temp_c1[1]), .Cin(temp_c2[1]), .Sum(temp_sum2[2]), .Cout(temp_c2[2]));
+
 fulladder bit0(.A(temp_sum1[0]), .B(1'b0), .Cin(1'b0), .Sum(Sum[0]), .Cout(temp_c2[0]));
 fulladder bit1(.A(temp_sum1[1]), .B(temp_c1[1]), .Cin(temp_c2[0]), .Sum(Sum[1]), .Cout(temp_c2[1]));
 fulladder bit2(.A(1'b0), .B(temp_c1[2]), .Cin(temp_c2[1]), .Sum(Cout), .Cout(temp_c2[2]));
+   
 endmodule
