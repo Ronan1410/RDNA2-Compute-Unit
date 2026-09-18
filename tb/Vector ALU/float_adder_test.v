efd5f405655b6cc4873d2f87b30c50ad3e2889a1
Ronan1410 <1410ronan@gmail.com>
2026-09-18 21:52:31 +0530

    fixed shifting error

--- tb/Vector ALU/float_adder_test.v
+++ tb/Vector ALU/float_adder_test.v
@@ -4,7 +4,6 @@ module float_adder_test;
 reg[31:0] A;
 reg[31:0] B;
 wire[31:0] out;
-
 wire NaN_flag;
 wire overflow_flag;
 
@@ -12,6 +11,7 @@ float_adder_32 test(.A(A), .B(B), .out(out), .NaN_flag(NaN_flag), .overflow_flag
 
 initial
 begin
+
     #(10);
     A = 32'b01111111100000000000000000000000;
     B = 32'b0;
@@ -33,7 +33,7 @@ begin
     #(10);
     B = 32'b01111111100000000000000000000000;
     A = 32'b0;
-        #(10);
+    #(10);
     $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
     
     #(10);
@@ -59,7 +59,8 @@ begin
     B = 32'b11111111100000000000000000000000;
     #(10);
     $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
-        #(10);
+    
+    #(10);
     A = 32'b01111111100000000000000000000000;
     B = 32'b01111111100000000000000000000000;
     #(10);
@@ -70,8 +71,7 @@ begin
     B = 32'b11111111100000000000000000000000;
     #(10);
     $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
-
-        
+    
     #(10);
     A = 32'd1;
     B = 32'd2;
@@ -79,15 +79,41 @@ begin
     $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
     
     #(10);
-    B = 32'b00000000100000000000000000000000;
     A = 32'b00000001100000000000000000000000;
+    B = 32'b00000000100000000000000000000000;
     #(10);
     $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
     
     #(10);
+    A = 32'b00000010100000000000000000000000;
     B = 32'b00000001100000000000000000000000;
-    A = 32'b00000001000000000000000000000000;
     #(10);
     $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
+    
+    #(10);
+    A = 32'b00000001100000000000000000000000;
+    B = 32'b00000001100000000000000000000000;
+    #(10);
+    $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
+    
+    #(10);
+    A = 32'b00000001100000000000000000000000;
+    B = 32'b00000011100000000000000000000000;
+    #(10);
+    $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
+    
+    #(10);
+    A = 32'b00000011000000000000000000000001;
+    B = 32'b00010011100000000000000000000000;
+    #(10);
+    $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);
+    
+    #(10);
+    A = 32'b00000001000000000000000000000000;
+    B = 32'b00000010000000000000000000000100;
+    #(10);
+    $display("A = %d, B = %d, out = %d, Nan_flag = %d, overflow = %d", A, B, out, NaN_flag, overflow_flag);    
+
 end
+
 endmodule
