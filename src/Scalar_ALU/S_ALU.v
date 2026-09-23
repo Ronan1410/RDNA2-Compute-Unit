0cd96f863708b2b2f9073d8438c30ff1c6a7787e
Ronan1410 <1410ronan@gmail.com>
2026-09-23 23:35:43 +0530

    Basic register file for scalar ALU

--- src/Scalar_ALU/S_ALU.v
+++ src/Scalar_ALU/S_ALU.v
@@ -1,7 +1,8 @@
 `timescale 1ns / 1ps
 
 module S_ALU(
-    instruction
+    instruction,
+    clock
 );
     //include the header files
     `include "scalars.vh"
@@ -10,6 +11,7 @@ module S_ALU(
 
     //input 64 bit instruction
     input [31:0] instruction;
+    input clock;
     
     wire [6:0] SDST;
     wire [7:0] SSRC1, SSRC0;
@@ -29,12 +31,31 @@ module S_ALU(
     assign SSRC1 = instruction[15:8];
     assign SIMM = instruction[15:0];
 
-    always @(instruction)
+    //select lines s0 and s1 for reading vlaues at r0 and r1
+    //select line w0 for writing value(wv) in the register file
+
+    wire [7:0] s0, s1, w0;
+    wire [63:0] wv, r0, r1, EXEC_in, EXEC_out;
+    wire en_w, VCCZ_in, VCCZ_out, en_64, SCC_in. SCC_out, EXECZ_in, EXECZ_out;
+
+    regFile regFIle(.s0(s0), .s1(s1),
+                    .w0(w0), .wv(wv),
+                    .clock(clock), .en_w(en_w),
+                    .r0(r0), r1(r1), .en_64(en_64),
+                    .VCCZ_in(VCCZ_in), .VCCZ_out(VCCZ_out),
+                    .SCC_in(SCC_in), .SCC_out(SCC_out),
+                    .EXECZ_in(EXECZ_in), .EXECZ_out(EXECZ_out),
+                    .EXEC_in(EXEC_in), .EXEC_out(EXEC_out));
+
+    always @(posedge clock)
     begin
         casex(op)
         //ALU operations for SOP1
             {SOP1}: begin
                 casex(SOP1_op)
+                S_MOV_B32: begin
+
+                end
 
                 endcase
             end
@@ -58,5 +79,11 @@ module S_ALU(
             end
         //ALU operations for SOP2
         endcase
+    end
+
+    //disabling write enabled
+    always @(negedge clock)
+    begin
+        en_w <= 0;
     end
 endmodule
---
+++ src/Scalar_ALU/regFile.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+module regFile(
+    input [7:0] s0,
+    input [7:0] s1,
+    input [7:0] w0,
+    input [63:0] wv,
+    input clock,
+    input en_w,
+    input en_64,
+    input VCCZ_in,
+    input EXECZ_in,
+    input SCC_in,
+    input [63:0] EXEC_in,
+    output reg [63:0] EXECZ_out,
+    output reg [63:0] r0,
+    output reg [63:0] r1,
+    output reg VCCZ_out,
+    output reg EXECZ_out,
+    output reg SCC_out
+);
+
+reg [31:0] register [255:0];
+always @(posedge clock)
+begin
+    if(!s0 == 8'hFF)
+    begin
+        r0 <= {register[s0 + 1], register[s0]};
+    end
+    else
+    begin
+        r0 <= {32'bx, register[s0]};
+    end
+    if(!s1 == 8'hFF)
+    begin
+        r1 <= {register[s1 + 1], register[s1]};
+    end
+    else
+    begin
+        r1 <= {32'bx, register[s1]};
+    end
+    //writing write value to the register expect for read only values
+    if(en_w && (!(w0 == 8'h7d || (w0 >= 8'h80 && w0 <= 8'he8) || (w0 >= 8'hF0 && w0 <= 8'hF8))))
+    begin
+        if(en_64)
+        begin
+            {register[w0 + 1], register[w0]} <= wv;
+        end
+        else 
+        begin
+            register[w0] <= wv[31:0];
+        end
+    end
+
+    //reading and writing VCCZ, SCC, and EXECZ
+    VCCZ_out <= register[8'hfb][0];
+    register[8'hfb] <= {30'b0, VCCZ_in};
+    SCC_out <= register[8'hfc][0];
+    register[8'hfc] <= {30'b0, SCC_in};
+    EXECZ_out <= register[8'hfd][0];
+    register[8'hfd] <= {30'b0, EXECZ_in};
+    //handling EXEC register
+    EXECZ_out <= {register[8'h70], register[8'h7e]};
+    if(EXECZ_in) register[8'h7f] <= EXEC_in;
+end
+endmodule
